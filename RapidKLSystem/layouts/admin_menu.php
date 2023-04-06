134a9d031caadfffbcbe39e78408e225478b01cb
MELZZ0602 <130069704+MELZZ0602@users.noreply.github.com>
2023-04-06 21:56:11 +0800

    Add files via upload

---
+++ RapidKLSystem/layouts/admin_menu.php
@@ -0,0 +1,51 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Category</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Products</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage Products</a> </li>
+       <li><a href="add_product.php">Add Products</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Image Gallery</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-credit-card"></i>
+       <span>Sales</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Manage Sales</a> </li>
+         <li><a href="add_sale.php">Add Sale</a> </li>
+     </ul>
+  </li>
+  <li>
+</ul>
---
+++ RapidKLSystem/layouts/special_menu.php
@@ -0,0 +1,14 @@
+<ul>
+  <li>
+    <a href="home.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Image Gallery</span>
+    </a>
+  </li>
+</ul>
